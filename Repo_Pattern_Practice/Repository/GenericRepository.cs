839032c8d4a3a8dff3770a485029412de5ac031d
PDMarschall <76184606+PDMarschall@users.noreply.github.com>
2021-11-14 12:29:57 +0100

    interface

---
+++ Repo_Pattern_Practice/Repository/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo_Pattern_Practice.Repository
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        public IEnumerable<T> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Select(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repo_Pattern_Practice/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo_Pattern_Practice.Repository
+{
+    public interface IRepository<T>
+    {
+        T Delete(T entity);
+        T Update(T entity);
+        T Insert(T entity);
+        IEnumerable<T> All();
+        IEnumerable<T> Select(Expression<Func<T, bool>> predicate);
+        void SaveChanges();
+
+    }
+}
