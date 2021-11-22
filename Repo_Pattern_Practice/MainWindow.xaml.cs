bed4103f0731eaa78f1db6e194552321121d48f7
Peter Marschall <PDMarschall@hotmail.com>
2021-11-22 19:33:41 +0100

    flere rows

--- Repo_Pattern_Practice/MainWindow.xaml.cs
+++ Repo_Pattern_Practice/MainWindow.xaml.cs
@@ -9,10 +9,56 @@ namespace ZipcodeEditor
 {
     public partial class MainWindow : Window
     {
+        private List<Zipcode> listZipcodes = new List<Zipcode>();
+        private List<Addresse> listAddresses = new List<Addresse>();
+        private ApplicationContext applicationContext;
+        private ZipcodeRepository zipcodeRepository;
+        private AddressRepository addressRepository;
 
         public MainWindow()
         {
+            InitializeComponent();
+            applicationContext = new ApplicationContext();
+            zipcodeRepository = new ZipcodeRepository(applicationContext);
+            addressRepository = new AddressRepository(applicationContext);
+            Select();
+        }
 
+        private void Select()
+        {
+            IEnumerable<Zipcode> searchResultsZipcode = zipcodeRepository.Select(zipcode => zipcode.Code.StartsWith(Zipcode_Search.Text.Trim())
+                                                                    && zipcode.City.StartsWith(City_Search.Text.Trim()));
+            zipcodeRepository.SaveChanges();
+            IEnumerable<Addresse> searchResultsAddress = addressRepository.Select(address => address.Phone.StartsWith(Phone_Search.Text.Trim()));
+            addressRepository.SaveChanges();
+
+            listZipcodes.Clear();
+            foreach (Zipcode result in searchResultsZipcode)
+            {
+                listZipcodes.Add(new Zipcode { 
+                    Code = result.Code.ToString(), 
+                    City = result.City.ToString() }); ;
+            }
+            listAddresses.Clear();
+            foreach (Addresse result in searchResultsAddress)
+            {
+                listAddresses.Add(new Addresse { 
+                    Phone = result.Phone.ToString(), 
+                    FirstName = result.FirstName.ToString(), 
+                    LastName = result.LastName.ToString(),
+                    Address = result.Address.ToString(),
+                    Email = result.Email.ToString(),
+                    Title = result.Title.ToString(),
+                    Zipcode = result.Zipcode.ToString(),
+                    City = result.City.ToString()}) ;
+            }
+
+            Refresh();
+        }
+        private void Refresh()
+        {
+            maingrid.ItemsSource = new ObservableCollection<Zipcode>(listZipcodes);
+            maingrid.ItemsSource = new ObservableCollection<Addresse>(listAddresses);
         }
 
 
@@ -28,6 +74,16 @@ namespace ZipcodeEditor
         }
 
         private void ClearFields_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmdSearch_Click(object sender, RoutedEventArgs e)
         {
 
         }
