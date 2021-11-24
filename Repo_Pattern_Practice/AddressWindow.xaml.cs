37ca0a0360f29d898cfc1d1142c1047faf2271b4
Peter Marschall <PDMarschall@hotmail.com>
2021-11-24 11:16:05 +0100

    nyt vindue

---
+++ Repo_Pattern_Practice/AddressWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using Repo_Pattern_Practice.Models;
+
+using Repo_Pattern_Practice.Repository;
+
+namespace ZipcodeEditor
+{
+    public partial class AddressWindow : Window
+    {
+        public AddressWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
