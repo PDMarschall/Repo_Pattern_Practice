e25bb6fcb0979b0b122a906f1f482535d50fa9bd
Peter Marschall <PDMarschall@hotmail.com>
2021-11-30 13:19:43 +0100

    warning - doesnt work yet

--- Repo_Pattern_Practice/RepositoryFactory.cs
+++ Repo_Pattern_Practice/RepositoryFactory.cs
@@ -1,32 +1,32 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Repo_Pattern_Practice.Repository;
-//using Repo_Pattern_Practice.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repo_Pattern_Practice.Repository;
+using Repo_Pattern_Practice.Models;
 
-//namespace Repo_Pattern_Practice
-//{
-//    public static class RepositoryFactory
-//    {       
-//        public static IRepository<T> GetRepository<T>(T obj)
-//        {
-//            IRepository<T> repository = null;
-            
+namespace Repo_Pattern_Practice
+{
+    public static class RepositoryFactory
+    {
+        public static IRepository<T> GetRepository<T>(T obj)
+        {
+            IRepository<T> repository = null;
 
-//            switch (obj)
-//            {
-//                case Addresse:
-//                    repository = new AddressRepository();
-//                    break;
-//                case Zipcode:
-//                    repository = new ZipcodeRepository();
-//                    break;
-//                default:
-//                    throw new ArgumentException("Invalid repository type");
-//            }
 
-//            return repository;
-//        }
-//    }
+            switch (obj)
+            {
+                case Addresse:
+                    repository = new AddressRepository();
+                    break;
+                case Zipcode:
+                    repository = new ZipcodeRepository();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid repository type");
+            }
+
+            return repository;
+        }
+    }
